97136e86c6da2edcd8d8a23d17a070f4d1fe20bb
José Emilio Sánchez Aparicio <joseemilio.sanchez@uab.cat>
2017-12-14 10:12:29 +0100

    Fix: open3align frame was not visible

--- chimera_p4/gui.py
+++ chimera_p4/gui.py
@@ -79,6 +79,7 @@ class p4Dialog(PlumeBaseDialog):
 		self.ui_input_frame.rowconfigure(1, weight=1)
 		self.ui_input_frame.columnconfigure(1, weight=1)
 		self.ui_o3align_btn = tk.Button(self.ui_o3align_frame, text='Align!', command=self._cmd_o3align_btn)
+		self.ui_input_frame.pack(expand=True, fill='both', padx=5, pady=5)
 
 		"""
 		#Third frame to perform pharmacophores
